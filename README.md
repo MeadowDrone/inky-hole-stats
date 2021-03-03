5cd7bf42918b644b231bdbc015c0e58ecf858092
Other <other@Erikas-MacBook-Pro.local>
2021-03-03 17:57:52 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# inky-hole-stats
-A heads-up display of system stats for Inky pHAT screens running Pi-Hole 
+# Inky Hole Stats
+
+Derived from [neauoire's Inky Hole](https://github.com/neauoire/inky-hole).
+
+Display the **number of blocked requests, and filtered traffic** from [Pi-Hole](https://pi-hole.net) along with current system uptime, RAM, disk space and temperature, on [Pimoroni's Inky-Phat](https://github.com/pimoroni/inky-phat/issues).
+
+<img src='https://raw.githubusercontent.com/MeadowDrone/inky-hole-stats/master/stats.jpg?v=1' width="600"/>
+
+- Setup **Pi-Hole**, follow the [installation instructions](https://learn.adafruit.com/pi-hole-ad-blocker-with-pi-zero-w/install-pi-hole).
+- Setup **InkyPhat**, follow the [installation instructions](https://learn.pimoroni.com/tutorial/sandyj/getting-started-with-inky-phat).
+- Clone this repo on your [Raspberry Pi Zero W](https://www.raspberrypi.org/products/).
+
+## Reload automatically every 30 minutes
+
+Edit `crontab`. 
+
+```
+crontab -e
+```
+
+Add the following line:
+
+```
+*/30 * * * * python /home/pi/inky-hole/main.py
+```
+
+Enjoy!
---
+++ stats.py
@@ -0,0 +1,82 @@
+import os
+import json
+import psutil
+import urllib.request
+from inky import InkyPHAT
+from PIL import Image, ImageFont, ImageDraw
+from font_fredoka_one import FredokaOne
+
+
+# SYSTEM INFO
+# Uptime
+uptime = os.popen('uptime -p').read()[3:-1]
+uptime_stripped = uptime[:uptime.index(',')]
+'''uptime_hours = uptime[uptime.index(',')+2:]
+uptime_hours = uptime_hours[:uptime_hours.index(',')]
+uptime_full = f'{uptime_days}, {uptime_hours}'''
+
+# Free RAM in MB
+ram_mb = int(psutil.virtual_memory().available) / 1024 / 1024
+ram_mb_str = str(round(ram_mb, 2)) + "MB"
+
+# Temperature in C
+temp = round(psutil.sensors_temperatures().get('cpu_thermal')[0].current, 1)
+temp_str = str(temp) + "C"
+
+# Disk space in GB
+disk = psutil.disk_usage('/').free
+disk_gb = round(disk / 1024 / 1024 / 1024, 2)
+disk_str = str(disk_gb) + "GB"
+
+# Set current directory
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+# Pi-hole API data
+try:
+    with urllib.request.urlopen('http://192.168.1.220/admin/api.php') as f:
+        parsed_json = json.loads(f.read())
+        ads_blocked = parsed_json['ads_blocked_today']
+        ratio_blocked = parsed_json['ads_percentage_today']
+        status = parsed_json['status']
+except Exception:
+    status = 'disabled'
+
+small_font = ImageFont.truetype(FredokaOne, 16)
+font = ImageFont.truetype(FredokaOne, 20)
+large_font = ImageFont.truetype(FredokaOne, 40)
+
+inky_display = InkyPHAT("red")
+inky_display.set_border(inky_display.WHITE)
+img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
+draw = ImageDraw.Draw(img)
+
+if status != 'enabled':
+    draw.text((20, 5), "Pi-hole", inky_display.RED, large_font)
+    draw.text((20, 45), "is down :(", inky_display.RED, large_font)
+else:
+    # Draw ads blocked
+    ads_text = str(ads_blocked) + " (" + str(
+            "%.1f" % round(ratio_blocked, 2) + "%)"
+            )
+    draw.text((5, 2), "Ads blocked:", inky_display.BLACK, small_font)
+    draw.text((110, 2), ads_text, inky_display.RED, small_font)
+
+    # Draw uptime
+    draw.text((5, 22), "Uptime:", inky_display.BLACK, small_font)
+    draw.text((110, 22), uptime_stripped, inky_display.RED, small_font)
+
+    # Draw RAM
+    draw.text((5, 42), "RAM (free):", inky_display.BLACK, small_font)
+    draw.text((110, 42), ram_mb_str, inky_display.RED, small_font)
+
+    # Draw disk space
+    draw.text((5, 62), "Disk Space:", inky_display.BLACK, small_font)
+    draw.text((110, 62), disk_str, inky_display.RED, small_font)
+
+    # Draw temperature
+    draw.text((5, 82), "Temp:", inky_display.BLACK, small_font)
+    draw.text((110, 82), temp_str, inky_display.RED, small_font)
+
+inky_display.set_image(img)
+
+inky_display.show()
